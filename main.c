ade55ed938a78b357b3d04bb748901138f36efcf
demian9079 <demian9079@gmail.com>
2023-04-09 23:49:15 +0600

    Nartkotta
    Nartkotta game

--- main.c
+++ main.c
@@ -1,98 +1,133 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 
-void print_board(char **board, int m, int n){
+#define MAX_ROWS 100
+#define MAX_COLS 100
+
+void print_board(char board[MAX_ROWS][MAX_COLS], int m, int n) {
     printf("  ");
-    for(int i = 0; i < n; i++){
+    for (int i = 0; i < n; i++) {
         printf("%d ", i+1);
     }
     printf("\n");
-    for(int i = 0; i < m; i++){
+
+    for (int i = 0; i < m; i++) {
         printf("%d ", i+1);
-        for(int j = 0; j < n; j++){
+        for (int j = 0; j < n; j++) {
             printf("%c ", board[i][j]);
         }
         printf("\n");
     }
 }
 
-int main(){
+int main() {
     int m, n;
-    printf("Введите кол-во строк: ");
+    printf("Введите количество строк: ");
     scanf("%d", &m);
-    printf("Введите кол-во столбцов: ");
+    printf("Введите количество столбцов: ");
     scanf("%d", &n);
 
-    char **board = (char **) malloc(m * sizeof(char *));
-    for(int i = 0; i < m; i++){
-        board[i] = (char *) malloc(n * sizeof(char));
-        for(int j = 0; j < n; j++){
-            if(j == 0) board[i][j] = 'X';
-            else if(j == n-1) board[i][j] = 'O';
-            else board[i][j] = '.';
+    char board[MAX_ROWS][MAX_COLS];
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (j == 0) {
+                board[i][j] = 'X';
+            }
+            else if (j == n-1) {
+                board[i][j] = 'O';
+            }
+            else {
+                board[i][j] = '.';
+            }
         }
     }
 
-    int run = 1;
     print_board(board, m, n);
+
     int player = 1;
-    int row, col;
-    char *text;
-    while(run){
+    int run = 1;
+    while (run) {
+        bool stopX[MAX_ROWS] = {0};
+        bool stopO[MAX_ROWS] = {0};
+        for (int i = 0; i < m; i++) {
+            if (board[i][0] == 'X' && board[i][1] == 'O') {
+                stopX[i] = true;
+            }
+            if (board[i][n-1] == 'O' && board[i][n-2] == 'X') {
+                stopO[i] = true;
+            }
+        }
+        bool flagX = true, flagO = true;
+        for (int i = 0; i < m; i++) {
+            flagX &= stopX[i];
+            flagO &= stopO[i];
+        }
+
+        if (flagO) {
+            printf("Победил X\n");
+            break;
+        }
+        if (flagX) {
+            printf("Победил O\n");
+            break;
+        }
+
         printf("Ход игрока %d\n", player);
-        if(player == 1){
+
+        int row, col;
+        char* text;
+        if (player == 1) {
             printf("Выберите X: ");
             scanf("%d", &row);
-            row--;
             printf("На какой столбец вы хотите походить: ");
             scanf("%d", &col);
-            col--;
-            text = board[row];
-            while(strstr(text, "X") == &text[col]){
-                printf("Ход не возможен\n");
+
+			col--;
+
+            text = board[row-1];
+            while (col == strchr(text, 'X') - text) {
+                printf("Ход невозможен\n");
                 print_board(board, m, n);
                 printf("На какой столбец вы хотите походить: ");
                 scanf("%d", &col);
-                col--;
             }
-            if(col >= (strstr(text, "O") - text)){
-                printf("Ход не возможен\n");
-            }else{
+            if (col >= strchr(text, 'O') - text) {
+                printf("Ход невозможен\n");
+            }
+            else {
                 player = 2;
-                text[strstr(text, "X") - text] = '.';
+                text[strchr(text, 'X') - text] = '.';
                 text[col] = 'X';
             }
             print_board(board, m, n);
-        }else{
+        }
+        else if (player == 2) {
             printf("Выберите O: ");
             scanf("%d", &row);
-            row--;
             printf("На какой столбец вы хотите походить: ");
             scanf("%d", &col);
-            col--;
-            text = board[row];
-            while(strstr(text, "O") == &text[col]){
-                printf("Ход не возможен\n");
+
+			col--;
+
+            text = board[row-1];
+            while (col == strchr(text, 'O') - text) {
+                printf("Ход невозможен\n");
                 print_board(board, m, n);
                 printf("На какой столбец вы хотите походить: ");
                 scanf("%d", &col);
-                col--;
             }
-            if(col <= (strstr(text, "X") - text)){
-                printf("Ход не возможен\n");
-            }else{
+            if (col <= strchr(text, 'X') - text) {
+                printf("Ход невозможен\n");
+            }
+            else {
                 player = 1;
-                text[strstr(text, "O") - text] = '.';
+                text[strchr(text, 'O') - text] = '.';
                 text[col] = 'O';
             }
             print_board(board, m, n);
         }
-    }
-    for(int i = 0; i < m; i++){
-        free(board[i]);
-    }
-    free(board);
-    return 0;
-    
+	}
+	return 0;
 }
